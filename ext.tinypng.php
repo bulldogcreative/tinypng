17054a4e762c1d0db61740cd9653950d552b609b
Levi Durfee <levi.durfee@gmail.com>
2017-12-01 14:57:13 -0500

    Start writing code for extension installation [#1]

--- ext.tinypng.php
+++ ext.tinypng.php
@@ -1,1 +1,36 @@
 <?php
+
+class Tinypng_ext
+{
+    public $name = 'TinyPNG';
+    public $version = '2.0.0';
+    public $description = 'Optimize your images for performance!';
+    public $settings_exist = 'y';
+    public $docs_url = 'https://github.com/BulldogCreative/tinypng/';
+
+    public $settings = array();
+
+    public function __construct($settings = array())
+    {
+        $this->settings = $settings;
+    }
+
+    public function activate_extension()
+    {
+        $this->settings = array(
+            'api_key' = '',
+        );
+
+        $data = array(
+            'class'    => __CLASS__,
+            'method'   => 'tiny',
+            'hook'     => 'file_after_save',
+            'settings' => serialize($this->settings),
+            'priority' => 10,
+            'version'  => $this->version,
+            'enabled'  => 'y',
+        );
+
+        ee()->db->insert('extensions', $data);
+    }
+}
